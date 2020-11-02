0b87077604717d5e3e83a0296c1c00a9b6d54ed8
simonoganesyan <simonoganesyan@gmail.com>
2020-11-02 16:43:06 +0400

    Add files via upload

---
+++ migrations/m180711_155549_create_facebook_channels_table.php
@@ -0,0 +1,20 @@
+<?php
+
+class m180711_155549_create_facebook_channels_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        /**  */
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function down()
+    {
+        /**  */
+    }
+}
